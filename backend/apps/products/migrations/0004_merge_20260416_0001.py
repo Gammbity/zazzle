623ea4c496fa427d538b8e035aa7ceb6d5803e79
Gammbity <abduboriyabdusamadov66@gmail.com>
2026-04-17 09:05:09 +0500

    feat: add migration to merge product variant changes

---
+++ backend/apps/products/migrations/0004_merge_20260416_0001.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0003_add_pen_product'),
+        ('products', '0003_remove_productvariant_unique_product_variant_and_more'),
+    ]
+
+    operations = []
